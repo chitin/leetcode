ddf7250aca0940b7b066e0e5dcdc49f94289d587
chitin <chitin1027@gmail.com>
2017-06-13 00:42:25 +0800

    Maximum Subarray add good way

--- src/main/java/leetcode056/Solution.java
+++ src/main/java/leetcode056/Solution.java
@@ -52,6 +52,15 @@ public class Solution {
         return maxResult;
     }
 
+    public static int maxSubArray4(int[] A) {
+        int max = A[0];
+        for (int i = 1; i < A.length; i++) {
+            A[i] = (A[i - 1] > 0 ? A[i - 1] : 0) + A[i];
+            max = Math.max(max, A[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
